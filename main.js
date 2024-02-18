ea17c80040aa6fb55d55edfea4de0d891be3ffec
Brayan Jesús Jurado Félix <153966576+Mitoonx@users.noreply.github.com>
2024-02-18 11:27:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ const connectionOptions = {
 	    version,
         printQRInTerminal: true,
         auth: state,
-        browser: ['senna-bot', 'Safari', '1.0.0'], 
+        browser: ['setax-bot', 'Safari', '1.0.0'], 
 	      patchMessageBeforeSending: (message) => {
                 const requiresPatch = !!(
                     message.buttonsMessage 
