6d51f71038fe59d5bdb9896fb72e27c414755c63
Brayan Jesús Jurado Félix <153966576+Mitoonx@users.noreply.github.com>
2024-02-23 14:04:01 -0500

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,16 +1,24 @@
+import { execSync } from 'child_process';
 
-import { execSync } from 'child_process'
 let handler = async (m, { conn, text }) => {
+    try {
+        // Utiliza un emoji específico en lugar de 'done'
+        m.react('✅');
 
-    m.react(done)
-if (conn.user.jid == conn.user.jid) {
-let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-//require('fs').readdirSync('plugins').map(v=>global.reload('', v))
-conn.reply(m.chat, stdout.toString(), m)
-}}
-handler.help = ['update']
-handler.tags = ['owner']
-handler.command = ['update', 'actualizar', 'fix', 'fixed'] 
-handler.rowner = true
+        if (conn.user.jid == conn.user.jid) {
+            let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+            //require('fs').readdirSync('plugins').map(v=>global.reload('', v))
+            conn.reply(m.chat, stdout.toString(), m);
+        }
+    } catch (error) {
+        // Maneja cualquier error que pueda ocurrir durante el proceso
+        conn.reply(m.chat, 'Error al actualizar: ' + error.message, m);
+    }
+}
 
-export default handler
+handler.help = ['update'];
+handler.tags = ['owner'];
+handler.command = ['update', 'actualizar', 'fix', 'fixed'];
+handler.rowner = true;
+
+export default handler;
