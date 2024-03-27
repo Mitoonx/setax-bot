a6e966cdc2fd76644759915d5e4a4a17aa321ebe
Brayan Jesús Jurado Félix <153966576+Mitoonx@users.noreply.github.com>
2024-03-26 22:10:27 -0500

    Update owner-addcred.js

--- plugins/owner-addcred.js
+++ plugins/owner-addcred.js
@@ -25,27 +25,31 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 *[🛑]* *CRÉDITOS ANTES:* *${user.credit - parseInt(txt)}*
 *[🛑]* *CREDITOS TOTAL:* *${user.credit}*
 *[🛑]* *RANGO:* *${rank}*
-*[🛑]* *ANTISPAM:* *${antispamDelay / 1000} SEGUNDOS*
+*[🛑]* *ANTI-SPAM:* *${antispamDelay / 1000} SEGUNDOS*
 └───────────────────────
 `, null, { mentions: [who] });
 }
 
 // Function to determine user rank based on credits
 function getRank(credit) {
-if (credit >= 1500) {
-  return "PLUS";
-} else if (credit >= 500) {
-  return "VIP";
-} else if (credit >= 1) {
-  return "STANDARD";
-} else {
-  return "FREE";
+  if (credit >= 100000) {
+     return "ADMINISTRADOR";
+  } else if (credit >= 1500) {
+     return "PLUS";
+  } else if (credit >= 500) {
+     return "VIP";
+  } else if (credit >= 1) {
+     return "STANDARD";
+  } else {
+     return "FREE";
 }
 }
 
 // Function to determine antispam delay based on rank
 function getAntispamDelay(rank) {
   switch (rank) {
+      case "ADMINISTRADOR":
+          return 0;
       case "PLUS":
           return 10 * 1000; // 10 seconds
       case "VIP":
